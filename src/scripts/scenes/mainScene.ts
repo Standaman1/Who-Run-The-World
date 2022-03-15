4d049fd170ad966e2afa64e40669357c0354341e
Stanley Chin <stanleyshrink@hotmail.com>
2022-03-15 15:08:08 +1100

    Character Loaded

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -1,4 +1,5 @@
-import { Scene3D } from '@enable3d/phaser-extension'
+import { enable3d, Scene3D, Canvas, THREE, ExtendedObject3D, PhysicsLoader } from '@enable3d/phaser-extension'
+import Phaser from 'phaser'
 
 export default class MainScene extends Scene3D {
   constructor() {
@@ -6,7 +7,8 @@ export default class MainScene extends Scene3D {
   }
 
   init() {
-    this.accessThirdDimension()
+    this.accessThirdDimension({antialias: true})
+    this.third.load.preload('grass', './assets/img/grass-texture-1.jpg')
   }
 
   create() {
@@ -19,12 +21,46 @@ export default class MainScene extends Scene3D {
     // adds a box with physics
     this.third.physics.add.box({ x: -1, y: 2 })
     this.third.physics.add.box({ x: -0.5, y: 1 })
-    this.third.physics.add.sphere({ x: -0.5, y: 5 }, {lambert: {color:'green'}})
+    this.third.physics.add.sphere({ x: -1, y: 5 }, {lambert: {color:'green'}})
+
+    //add a texture
+    this.third.load.texture('grass').then(grass => {
+      grass.wrapS = grass.wrapT = 1000 // RepeatWrapping
+      grass.offset.set(0, 0)
+      grass.repeat.set(2, 2)
+
+      // BUG: To add shadows to your ground, set transparent = true
+      this.third.physics.add.ground({ width: 20, height: 20, y: 0 }, { phong: { map: grass, transparent: true } })
+    })
+
+    //add a robot
+    this.third.load.gltf('./assets/low_poly_character_kit_animation/scene.gltf').then(gltf => {
+      const child = gltf.scene.children[0]
+
+      const character = new ExtendedObject3D()
+      character.add(child)
+      this.third.scene.add(character)
+
+      let i = 0
+      let anims = ['run', 'sprint', 'jump_running', 'idle', 'driving', 'falling']
+
+      // ad the box man's animation mixer to the animationMixers array (for auto updates)
+      this.third.animationMixers.add(character.anims.mixer)
+
+      gltf.animations.forEach(animation => {
+        if (animation.name) {
+          // add a new animation to the box man
+          character.anims.add(animation.name, animation)
+        }
+      })
+
+    // setTimeout(() => {
+    //   this.scene.restart()
+    // }, 60000)
 
 
-
-    // throws some random object on the scene
+  //   // throws some random object on the scene
+  })
   }
-
   update() {}
 }
