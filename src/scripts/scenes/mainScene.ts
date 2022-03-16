4f761b33bb1cad7c363480888b2f5c6484568161
Stanley Chin <stanleyshrink@hotmail.com>
2022-03-16 16:54:16 +1100

    Added Other World but probably will not use

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -1,4 +1,4 @@
-import { enable3d, Scene3D, Canvas, THREE, ExtendedObject3D, PhysicsLoader } from '@enable3d/phaser-extension'
+import { enable3d, Scene3D, Canvas, THREE, ExtendedObject3D, PhysicsLoader, ThirdPersonControls, PointerLock, PointerDrag } from '@enable3d/phaser-extension'
 import Phaser from 'phaser'
 
 export default class MainScene extends Scene3D {
@@ -9,21 +9,46 @@ export default class MainScene extends Scene3D {
   init() {
     this.accessThirdDimension({antialias: true})
     this.third.load.preload('grass', './assets/img/grass-texture-1.jpg')
+    this.third.renderer.outputEncoding = THREE.LinearEncoding
   }
 
-  create() {
-    // creates a nice scene
-    this.third.warpSpeed()
+  // async preLoad(){
+  //   const swamp = this.third.load.preload('swamp', './assets/swamp_location/scene.gltf')
 
-    // adds a box
-    this.third.add.box({ x: 1, y: 2 })
+  //   const robot = this.third.load.preload('robot', './assets/low_poly_character_kit_animation/robot.gltf')
 
-    // adds a box with physics
+  //   await Promise.all([swamp, robot])
+  // }
+
+  async create() {
+    
+    //Simple Map##############
+    const total = await this.third.warpSpeed()
+    console.log('warpSpeed', total)
+    // this.third.warpSpeed()
+    // this.lights
+    // const { lights } = this.third.warpSpeed('-ground', '-orbitControls')
+    ;(await this.third.warpSpeed()).camera
+
+  
+    // Creates allMap ##################
+  //   this.third.load.gltf('./assets/swamp_location/scene.gltf').then(object => {
+  //     const scene = object.scenes[0]
+  //     const allMap = new ExtendedObject3D()
+  //     allMap.name = 'scene'
+  //     allMap.add(scene)
+  //     this.third.add.existing(allMap)
+  // })
+    // })
+    // creates map
+
+
+    // adds a box with physics ##################
     this.third.physics.add.box({ x: -1, y: 2 })
     this.third.physics.add.box({ x: -0.5, y: 1 })
     this.third.physics.add.sphere({ x: -1, y: 5 }, {lambert: {color:'green'}})
 
-    //add a texture
+    // add a texture ################
     this.third.load.texture('grass').then(grass => {
       grass.wrapS = grass.wrapT = 1000 // RepeatWrapping
       grass.offset.set(0, 0)
@@ -34,33 +59,50 @@ export default class MainScene extends Scene3D {
     })
 
     //add a robot
-    this.third.load.gltf('./assets/low_poly_character_kit_animation/scene.gltf').then(gltf => {
+    this.third.load.gltf('./assets/low_poly_character_kit_animation/robot.gltf').then(gltf => {
       const child = gltf.scene.children[0]
 
       const character = new ExtendedObject3D()
       character.add(child)
       this.third.scene.add(character)
 
-      let i = 0
-      let anims = ['run', 'sprint', 'jump_running', 'idle', 'driving', 'falling']
+      // let i = 0
+      // let anims = ['idle', 'walk', 'run', 'jump_start', 'fall', 'reception']
 
       // ad the box man's animation mixer to the animationMixers array (for auto updates)
-      this.third.animationMixers.add(character.anims.mixer)
+      // this.third.animationMixers.add(character.anims.mixer)
 
-      gltf.animations.forEach(animation => {
-        if (animation.name) {
-          // add a new animation to the box man
-          character.anims.add(animation.name, animation)
-        }
-      })
+
+      //Animation of character- ------
+
+      // gltf.animations.forEach(animation => {
+      //   if (animation.name) {
+      //     // add a new animation to the box man
+      //     character.anims.add(animation.name, animation)
+      //   }
+      // })
+
+      // gltf.animations.forEach(clip => {
+      //   character.anims.mixer.clipAction(clip).reset().play()
+      // })
+
+      this.third.animationMixers.add(character.anims.mixer)
+            gltf.animations.forEach(animation => {
+              if (animation.name) {
+                character.anims.add(animation.name, animation)
+              }
+            })
+            character.anims.play('walk')
 
     // setTimeout(() => {
     //   this.scene.restart()
     // }, 60000)
 
 
-  //   // throws some random object on the scene
-  })
-  }
+  
+    })//robot added
+
+  }//create()
+
   update() {}
 }
--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -6,6 +6,7 @@ export default class PreloadScene extends Phaser.Scene {
   preload() {}
 
   create() {
+    
     this.scene.start('MainScene')
 
     /**
