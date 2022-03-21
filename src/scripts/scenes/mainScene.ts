8a7b292cca5daf337f2ed532bf75a462f9762fb1
Stanley Chin <stanleyshrink@hotmail.com>
2022-03-22 01:26:38 +1100

    Cleanup. play with sphere, sven files added

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -14,54 +14,49 @@ export default class MainScene extends Scene3D {
   character = new ExtendedObject3D;
   camerasArr = new Array;
   newTree = new ExtendedObject3D;
-  // box = new ExtendedObject3D();
-  // sphere = new ExtendedObject3D();
   treeCollide = false
+  earthCollide = false
+
   newStump = new ExtendedObject3D;
   sun = new ExtendedObject3D;
   earth = new ExtendedObject3D;
+  sven = new ExtendedObject3D;
+
 
 
 
   stumpCreate(xPos, yPos, zPos){
-      this.third.load.gltf('./assets/stump/scene.gltf').then(stump => {
-        
-        console.log('stump', stump.scene.children[0])
-        const child = stump.scene.children[0];
-  
-        this.newStump.add(child)
-        // this.newStump.visible = false
-        this.newStump.position.set(xPos, yPos, zPos)
-  
-        this.newStump.name = 'stumpy'
-        
-        this.third.physics.add.existing(this.newStump, { 
-          shape: 'box', 
-          offset: { y: -0.5 }, 
-          width: 2,
-          height: 2,
-          depth: 2
-        })
-        this.newStump.body.setCollisionFlags(1)
-        this.third.add.existing(this.newStump)
-        
-      })
+    this.third.load.gltf('./assets/stump/scene.gltf').then(stump => {
       
-      this.newStump.scale.set(0.2, 0.2, 0.2)
-      console.log('this.newStump', this.newStump)
-    console.log('stumpcreate()', xPos, yPos, zPos)
+      console.log('stump', stump.scene.children[0])
+      const child = stump.scene.children[0];
+
+      this.newStump.add(child)
+      // this.newStump.visible = false
+      this.newStump.position.set(xPos, yPos, zPos)
+
+      this.newStump.name = 'stumpy'
+      
+      this.third.physics.add.existing(this.newStump, { 
+        shape: 'box', 
+        offset: { y: -0.5 }, 
+        width: 2,
+        height: 2,
+        depth: 2
+      })
+      this.newStump.body.setCollisionFlags(1)
+      this.third.add.existing(this.newStump)
+      
+    })
+    
+    this.newStump.scale.set(0.3, 0.3, 0.3)
 
   }
-  
-  // delta = {};
   
 
   init() {
     this.accessThirdDimension({antialias: true, gravity: { x: 0, y: -20, z: 0 }});
     this.third.load.preload('grass', './assets/img/grass-texture-1.jpg');
-    // this.third.load.preload('backgroundMountain', './assets/background_screen/background_mountain.png');
-
-
     this.third.renderer.outputEncoding = THREE.LinearEncoding;
     let canJump = true
     
@@ -70,19 +65,15 @@ export default class MainScene extends Scene3D {
   async create() {
     
     //Simple Map##############
-    // const {lights} = await this.third.warpSpeed();
     let total = await this.third.warpSpeed('-ground');
-    // console.log('warpSpeed', total);
     let directionalLight = this.third.lights.directionalLight();
     directionalLight.intensity = 0.5;
     directionalLight.color.setRGB(0, 0, 1)
 
-    // console.log('directionalLight', directionalLightIntensity)
-
+    //Add Sun + Earth 
     this.sun = this.third.physics.add.sphere({ x: 2, y: 2, z: 1, radius: 1 }, { basic: { color: 0xffff00 } })
     this.earth = this.third.physics.add.sphere({ y: 1, radius: 1 }, { basic: { color: 0x0080ff } })
 
-    // both are kinematic objects
     this.sun.body.setCollisionFlags(2)
     this.earth.body.setCollisionFlags(2)
   
@@ -118,24 +109,46 @@ export default class MainScene extends Scene3D {
 
   // })
     // creates map
-    
-    //add background image
-    // this.third.load.texture('backgroundMountain').then(backgroundMountain => {
-    //   // change encoding to LinearEncoding
-    //   backgroundMountain.encoding = THREE.LinearEncoding
-    //   backgroundMountain.needsUpdate = true
-    //   this.third.scene.background = backgroundMountain
+
+    //  //Add Sven ##################
+    //  await this.third.load.gltf('./assets/sven_walking/scene.gltf').then(svenObject => {
+
+
+    //   console.log('stump', svenObject)
+    //   const child = svenObject.scene.children[0];
+
+    //   this.sven.add(child)
+    //   this.sven.name = 'sven'
+      
+    //   this.third.physics.add.existing(this.sven, { 
+    //     shape: 'box', 
+    //     offset: { y: -0.5 }, 
+    //     width: 2,
+    //     height: 2,
+    //     depth: 2
+    //   })
+    //   this.sven.body.setCollisionFlags(2)
+    //   this.third.add.existing(this.sven)
+    //   this.third.animationMixers.add(this.sven.anims.mixer)
+    //   svenObject.animations.forEach(animation => {
+    //     if (animation.name) {
+    //       this.sven.anims.add(animation.name, animation)
+    //     }
+    //   })
+    //   this.sven.anims.play('walk')
+    //   console.log('this.sven', this.sven)
+      
     // })
+    
+    // this.sven.scale.set(0.2, 0.2, 0.2)
 
-    //Add tree
 
-    // const newTree = new ExtendedObject3D()
 
+    //Add tree ##################
     await this.third.load.fbx('./assets/img/tree.fbx').then(tree => {
-      console.log('tree', tree)
       this.newTree.add(tree)
       this.newTree.position.set(5, 1, 5)
-      this.newTree.name = 'treeCollide'
+      this.newTree.name = 'treefiddy'
       
       this.third.add.existing(this.newTree)
       this.third.physics.add.existing(this.newTree, { 
@@ -149,24 +162,15 @@ export default class MainScene extends Scene3D {
 
     })
 
-    this.newTree.scale.set(0.003, 0.003, 0.003)
-
-    // this.stumpCreate(xPos, yPos, zPos)
-
-   
-      // return this.newStump
-    
-
-    //Add Stump
-    
+    this.newTree.scale.set(0.003, 0.003, 0.003)    
 
 
-    // adds a box with physics ##################
+    // Adds some objects with physics ##################
     this.third.physics.add.box({ x: -1, y: 2 })
     this.third.physics.add.box({ x: -0.5, y: 1 })
     this.third.physics.add.sphere({ x: -1, y: 3 }, {lambert: {color:'green'}})
 
-    // add a texture ################
+    // Add a texture ################
     this.third.load.texture('grass').then(grass => {
       grass.wrapS = grass.wrapT = 1000 // RepeatWrapping
       grass.offset.set(0, 0)
@@ -175,12 +179,12 @@ export default class MainScene extends Scene3D {
       this.third.physics.add.ground({ width: 25, height: 25, y: 0 }, { phong: { map: grass, transparent: true } })
     })
 
-    
+    //Camera Position
     this.third.camera.position.set(-5, 10, -25)
     this.third.camera.lookAt(0, 0, 0)
 
 
-    //add a robot----------------------------
+    //Add a character----------------------------
     this.third.load.gltf('./assets/low_poly_character_kit_animation/scene.gltf').then(gltf => {
       
       const child = gltf.scene.children[0];
@@ -192,9 +196,7 @@ export default class MainScene extends Scene3D {
       }) //traverse to remove accessories
 
       
-      // const character = new ExtendedObject3D();
-      console.log('child', child);
-      // this.character.rotateY(Math.PI + 0.1)
+      // console.log('child', child);
       this.character.add(child);
       this.character.name = 'Dan'
       this.character.position.set(-1, 5, 0)
@@ -231,16 +233,16 @@ export default class MainScene extends Scene3D {
     this.character.body.setAngularVelocity(0, 0, 0)
 
 
+    //Quick Force Detection i.e bullets
     this.character.body.setCcdMotionThreshold(1e-7)
     this.character.body.setCcdSweptSphereRadius(0.25)
-    // this.character.body.setVelocity(0,0,0)
 
     const sensor = new ExtendedObject3D()
     sensor.position.setY(-0.625)
     this.third.physics.add.existing(sensor, { mass: 1e-10, shape: 'box', width: 0.2, height: 0.2, depth: 0.2 })
     sensor.body.setCollisionFlags(4)
 
-    // connect sensor to robot
+    // Connect Sensor to Character
     this.third.physics.add.constraints.lock(this.character.body, sensor.body)
 
 
@@ -250,15 +252,13 @@ export default class MainScene extends Scene3D {
       
     })
 
-    // const isTouchDevice =  true
-    // if (isTouchDevice) {
+    //Pointer Lock and Drag
+  
     //   const pointerLock = new PointerLock(this.game.canvas)
     //   const pointerDrag = new PointerDrag(this.game.canvas)
     //   pointerDrag.onMove(delta => {
-    //     if (!pointerLock.isLocked()) return
     //     const { x, y } = delta
     //   })
-    // }
 
     // controls.theta = 90
     
@@ -267,20 +267,28 @@ export default class MainScene extends Scene3D {
   //Add Camera Fixed to Body------------------
 
   const followCam = new THREE.Object3D()
-  // copies the position of the default camera
+  // Copies the Position of the Default Camera
   followCam.position.copy(this.third.camera.position)
   this.character.add(followCam)
   this.camerasArr.push(followCam)
 
   //Add Collision Detection Events
 
-  this.third.physics.add.collider(this.newTree, this.character, event=>{
-    console.log('Collided')
-  })
+  // this.third.physics.add.collider(this.newTree, this.character, event=>{
+  //   console.log('Collided')
+  // })
   this.newTree.body.on.collision((otherObject, event) => {
     if (otherObject.name === 'Dan'){
       // console.log(`tree and ${otherObject.name}: ${event}`)
       this.treeCollide = true
+      
+    }
+  })
+
+  this.earth.body.on.collision((otherObject, event) => {
+    if (otherObject.name === 'Dan'){
+      // console.log(`tree and ${otherObject.name}: ${event}`)
+      this.earthCollide = true
       
     }
   })
@@ -296,14 +304,14 @@ export default class MainScene extends Scene3D {
     
     //Sun and Earth Interplay
     const orbitRadius = 5
-    const date = time * 0.0025
+    const rotationAngle = time * 0.0015
     const { x, y, z } = this.earth.position.clone()
 
-    // make object orbit around a center
+    // Make Object Orbit
     this.earth.position.set(
-      Math.cos(date) * orbitRadius + this.sun.position.x,
+      Math.cos(rotationAngle) * orbitRadius + this.sun.position.x,
       y,
-      Math.sin(date) * orbitRadius + this.sun.position.z
+      Math.sin(rotationAngle) * orbitRadius + this.sun.position.z
     )
     
     const angle = Math.atan2(this.sun.position.x - x, this.sun.position.z - z)
@@ -313,7 +321,7 @@ export default class MainScene extends Scene3D {
 
     this.earth.body.needUpdate = true
   
-  }
+  }//end earth rotation
 
     const keys = {
       w: this.input.keyboard.addKey('w'),
@@ -327,7 +335,7 @@ export default class MainScene extends Scene3D {
 
     const v3 = new THREE.Vector3()
 
-    //Camera Lerp
+    //Camera Lerp + Follow
     const cameraArray = this.third.camera.getWorldPosition(v3)
     this.third.camera.position.lerp(
       cameraArray,
@@ -345,8 +353,6 @@ export default class MainScene extends Scene3D {
       let theta = Math.atan2(rotation.x, rotation.z)
       const rotationCharacter = this.character.getWorldDirection(v3)
       let thetaCharacter = Math.atan2(rotationCharacter.x, rotationCharacter.z)
-      // console.log('this.charccher', this.character.body.angularVelocity())
-      // this.character.body.setAngularVelocityY(0)
       
       const l = Math.abs(theta - thetaCharacter)
       // console.log('theta', theta)
@@ -368,23 +374,23 @@ export default class MainScene extends Scene3D {
 
 
 
-    if (keys.w.isDown) {
-      // this.character.body.setAngularVelocityY(0)
+      if (keys.w.isDown) {
+        // this.character.body.setAngularVelocityY(0)
 
-      if (this.character.anims.current === 'idle'){
-        this.character.anims.play('run', 15, true)
+        if (this.character.anims.current === 'idle'){
+          this.character.anims.play('run', 10000, true)
 
-        let x = Math.sin(theta) * speed
-        let y = this.character.body.velocity.y
-        let z = Math.cos(theta) * speed
-        this.character.body.setVelocity(x, y, z)
-      } 
-
-      } else {
-        if (this.character.anims.current === 'run'){
-          this.character.anims.play('idle')
+          let x = Math.sin(theta) * speed
+          let y = this.character.body.velocity.y
+          let z = Math.cos(theta) * speed
+          this.character.body.setVelocity(x, y, z)
         } 
-      }
+
+        } else {
+          if (this.character.anims.current === 'run'){
+            this.character.anims.play('idle')
+          } 
+        }
 
       if (keys.s.isDown) {
 
@@ -404,52 +410,54 @@ export default class MainScene extends Scene3D {
           } 
         }
 
-        if (keys.a.isDown) {
+      if (keys.a.isDown) {
 
-          if (this.character.anims.current === 'idle'){
-            this.character.anims.play('run')
-            theta *= -1
-            
-            let x = Math.cos(theta) * speed
-            let y = this.character.body.velocity.y
-            let z = Math.sin(theta) * speed
-            this.character.body.setVelocity(x, y, z)
+        if (this.character.anims.current === 'idle'){
+          this.character.anims.play('run')
+          theta *= -1
+          
+          let x = Math.cos(theta) * speed
+          let y = this.character.body.velocity.y
+          let z = Math.sin(theta) * speed
+          this.character.body.setVelocity(x, y, z)
+        } 
+  
+        } else {
+          if (this.character.anims.current === 'run'){
+            this.character.anims.play('idle')
           } 
-    
-          } else {
-            if (this.character.anims.current === 'run'){
-              this.character.anims.play('idle')
-            } 
-          }
+        }
 
-          if (keys.d.isDown) {
+      if (keys.d.isDown) {
 
-            if (this.character.anims.current === 'idle'){
-              this.character.anims.play('run')
+        if (this.character.anims.current === 'idle'){
+          this.character.anims.play('run')
 
-              // theta *= -1
-              
-              let x = Math.cos(theta) * speed * -1
-              let y = this.character.body.velocity.y
-              let z = Math.sin(theta) * speed
-              this.character.body.setVelocity(x, y, z)
-            } 
-      
-            } else {
-              if (this.character.anims.current === 'run'){
-                this.character.anims.play('idle')
-              } 
-            }
+          // theta *= -1
+          
+          let x = Math.cos(theta) * speed * -1
+          let y = this.character.body.velocity.y
+          let z = Math.sin(theta) * speed
+          this.character.body.setVelocity(x, y, z)
+        } 
+  
+        } else {
+          if (this.character.anims.current === 'run'){
+            this.character.anims.play('idle')
+          } 
+        }
 
           
-            let canJump =  true
+      let canJump =  true
     
       if(Phaser.Input.Keyboard.JustDown(keys.space)){
         
         canJump = false
-        this.character.anims.play('jump_start', 500, false)
+        this.character.anims.play('jump_start', 200, true)
+        this.character.anims.play('fall', 200, false)
+
         this.time.addEvent({
-          delay: 300,
+          delay: 600,
           callback: ()=>{
             canJump = true
             this.character.anims.play('idle')
@@ -463,11 +471,31 @@ export default class MainScene extends Scene3D {
         this.character.anims.play('interact', 300, false)
         console.log('chopped')
         const treePos = this.newTree.getWorldPosition(v3)
-        this.treeCollide = false
         this.third.destroy(this.newTree)
         // this.third.scene.add(this.newStump)
         // this.newStump.body.setPosition(treePos.x, treePos.y + 1, treePos.z)
+        this.treeCollide = false
         this.stumpCreate(treePos.x, treePos.y, treePos.z)
+        this.time.addEvent({
+          delay: 600,
+          callback: ()=>{
+            this.character.anims.play('idle')
+            
+          }
+
+        })
+
+      }
+
+      if(keys.c.isDown && this.earthCollide){
+        this.character.anims.play('push', 100, false)
+        console.log('pushed')
+        // this.third.scene.add(this.newStump)
+        // this.newStump.body.setPosition(treePos.x, treePos.y + 1, treePos.z)
+        const pushEarth = this.character.getWorldDirection(v3)
+        console.log(pushEarth)
+        this.character.body.applyForce(pushEarth.x * -5, pushEarth.y * -5, pushEarth.z * -5)
+        this.earthCollide = false
         this.time.addEvent({
           delay: 300,
           callback: ()=>{
@@ -476,7 +504,6 @@ export default class MainScene extends Scene3D {
           }
 
         })
-        // console.log('treePos', treePos)
 
       }
 
